f64b2cfeec8940a71f0d109d8e9f05f5ecbd3249
jeeneep <z1365244z@gmail.com>
2024-09-14 14:34:29 +0900

    init

---
+++ src/main/java/org/example/backedkickunity/BackedKickUnityApplication.java
@@ -0,0 +1,13 @@
+package org.example.backedkickunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackedKickUnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackedKickUnityApplication.class, args);
+    }
+
+}
