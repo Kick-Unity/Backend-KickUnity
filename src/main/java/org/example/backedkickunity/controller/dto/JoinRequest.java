51abcfc455f5d90f4814843863397a8ad25f9b5e
jeeneep <z1365244z@gmail.com>
2024-09-24 19:03:26 +0900

    01 feat : check~

--- src/main/java/org/example/backedkickunity/controller/dto/JoinRequest.java
+++ src/main/java/org/example/backedkickunity/controller/dto/JoinRequest.java
@@ -1,8 +1,13 @@
 package org.example.backedkickunity.controller.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JoinRequest {
 
     private String memberId;
--- src/main/java/org/example/backedkickunity/domain/member/exception/MemberExceptionType.java
+++ src/main/java/org/example/backedkickunity/domain/member/exception/MemberExceptionType.java
@@ -11,8 +11,8 @@ public enum MemberExceptionType implements BaseExceptionType {
     * */
     ALREADY_EXIST_MEMBER_ID(HttpStatus.CONFLICT,"JOIN_001", "이미 가입된 아이디입니다."),
     ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT,"JOIN_002", "이미 가입된 이메일입니다."),
-    INVALID_MEMBER_ID(HttpStatus.CONFLICT,"JOIN_003", "잘못된 형식의 아이디입니다."),
-    INVALID_MEMBER_PASSWORD(HttpStatus.CONFLICT,"JOIN_004", "잘못된 형식의 비밀번호입니다."),
+    INVALID_MEMBER_ID_FORMAT(HttpStatus.CONFLICT,"JOIN_003", "잘못된 형식의 아이디입니다."),
+    INVALID_PASSWORD_FORMAT(HttpStatus.CONFLICT,"JOIN_004", "잘못된 형식의 비밀번호입니다."),
     INVALID_EMAIL_FORMAT(HttpStatus.CONFLICT,"JOIN_005", "잘못된 형식의 이메일입니다."),
 
     MEMBER_NOT_EXIST(HttpStatus.CONFLICT, "MEMBER_001", "멤버가 존재하지 않습니다."),
